4811d79cec3fd391f083ca6a22e939d67600015d
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-12 00:46:18 +0530

    1st commit

---
+++ test.js
@@ -0,0 +1,8 @@
+const Employee = require("./employee");
+
+const emp = new Employee("John Wick", "Dog Lover")
+
+
+setTimeout(emp.sayName.bind(emp), 2000);
+
+setTimeout(emp.sayOccupation.bind(emp), 3000);
